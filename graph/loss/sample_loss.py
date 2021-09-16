aa5871f99baaddbac41d882fa7464b2a55599677
dollking <dollking@kookmin.ac.kr>
2021-09-16 14:53:59 +0900

    start

--- graph/loss/sample_loss.py
+++ graph/loss/sample_loss.py
@@ -12,6 +12,6 @@ class Loss(nn.Module):
     def forward(self, origin_code, trans_code, origin_feature, trans_feature, weight1=0.1, weight2=0.001):
         code_similar = torch.mean(torch.sum((trans_code != origin_code).float(), dim=1))
 
-        feature_similar = (self.loss(origin_feature, origin_code.detach) +
-                           self(trans_feature, trans_code.detach())) / 2
+        feature_similar = (self.loss(origin_feature, origin_code.detach()) +
+                           self.loss(trans_feature, trans_code.detach())) / 2
         return code_similar + weight2 * feature_similar
